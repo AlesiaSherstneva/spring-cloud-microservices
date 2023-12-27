fc6363d6abbef1ec056ad0d0896d3b2bcb9f2be5
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-12-28 02:35:13 +0300

    Create the controller with base CRUD operations

---
+++ mobile-app-ws/src/main/java/com/develop/app/ws/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.develop.app.ws.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+    @GetMapping
+    public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
+                           @RequestParam(value = "limit", defaultValue = "50") int limit,
+                           @RequestParam(value = "sort", required = false) String sort) {
+        return "get all users with page = " + page + " and limit = " + limit + " and sort = " + sort;
+    }
+
+    @GetMapping("/{userId}")
+    public String getUser(@PathVariable String userId) {
+        return "get user with id " + userId;
+    }
+
+    @PostMapping
+    public String createUser() {
+        return "create user";
+    }
+
+    @PutMapping
+    public String updateUser() {
+        return "update user";
+    }
+
+    @DeleteMapping
+    public String deleteUser() {
+        return "delete user";
+    }
+}
