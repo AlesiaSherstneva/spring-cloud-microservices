f83421571226cbbc9cf183d556987d0bf8322e0f
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-12-29 19:50:12 +0300

    Make POST method working with request body

--- mobile-app-ws/src/main/java/com/develop/app/ws/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/develop/app/ws/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.develop.app.ws.controller;
 
+import com.develop.app.ws.model.UserDetails;
 import com.develop.app.ws.model.UserRest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,9 +36,14 @@ public class UserController {
         return new ResponseEntity<>(returnValue, HttpStatus.OK);
     }
 
-    @PostMapping
-    public String createUser() {
-        return "create user";
+    @PostMapping(consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<UserRest> createUser(@RequestBody UserDetails userDetails) {
+        UserRest returnValue = new UserRest();
+        returnValue.setEmail(userDetails.getEmail());
+        returnValue.setFirstName(userDetails.getFirstName());
+        returnValue.setLastName(userDetails.getLastName());
+        return new ResponseEntity<>(returnValue, HttpStatus.OK);
     }
 
     @PutMapping
