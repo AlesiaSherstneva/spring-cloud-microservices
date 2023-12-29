f42c27c21e9213de853b790584a2e1da838b2211
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-12-29 18:35:03 +0300

    Create POJO class for user

---
+++ mobile-app-ws/src/main/java/com/develop/app/ws/model/UserRest.java
@@ -0,0 +1,17 @@
+package com.develop.app.ws.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String userId;
+}
