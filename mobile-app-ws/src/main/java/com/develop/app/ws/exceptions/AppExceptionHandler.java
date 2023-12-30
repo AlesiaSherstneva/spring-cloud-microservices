12c6ee68c646c1ea6e9c79790e2b66cd7dfd155c
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-12-30 23:13:39 +0300

    Create class for custom error message

--- mobile-app-ws/src/main/java/com/develop/app/ws/exceptions/AppExceptionHandler.java
+++ mobile-app-ws/src/main/java/com/develop/app/ws/exceptions/AppExceptionHandler.java
@@ -5,14 +5,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
 
 @ControllerAdvice
 public class AppExceptionHandler extends ResponseEntityExceptionHandler {
-
     @ExceptionHandler(value = {Exception.class})
-    public ResponseEntity<Object> handleAnyException(Exception exception, WebRequest request) {
-        return new ResponseEntity<>(exception, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<Object> handleAnyException(Exception exception) {
+        String errorMessageDescription = exception.getLocalizedMessage();
+
+        if (errorMessageDescription == null) {
+            errorMessageDescription = exception.toString();
+        }
+
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), errorMessageDescription);
+        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+
 }
