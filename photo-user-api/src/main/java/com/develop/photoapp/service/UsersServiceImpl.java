fcb9b227c3096caed8037d227279be5638131f1c
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-01-04 18:43:36 +0300

    Inject repository and mapper into service

--- photo-user-api/src/main/java/com/develop/photoapp/service/UsersServiceImpl.java
+++ photo-user-api/src/main/java/com/develop/photoapp/service/UsersServiceImpl.java
@@ -1,15 +1,30 @@
 package com.develop.photoapp.service;
 
+import com.develop.photoapp.entity.UserEntity;
+import com.develop.photoapp.repository.UsersRepository;
 import com.develop.photoapp.shared.UserDTO;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class UsersServiceImpl implements UsersService {
+    private final UsersRepository usersRepository;
+    private final ModelMapper modelMapper;
+
     @Override
     public UserDTO createUser(UserDTO userDetails) {
         userDetails.setUserId(UUID.randomUUID().toString());
+
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
+        userEntity.setEncryptedPassword("test");
+
+        usersRepository.save(userEntity);
         return null;
     }
 }
