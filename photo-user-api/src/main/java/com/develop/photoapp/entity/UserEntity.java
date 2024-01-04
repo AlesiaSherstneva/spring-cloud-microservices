99b55cc4087e774c7c73fa0b1e09cc4977f7534a
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-01-04 18:23:53 +0300

    Create user entity

---
+++ photo-user-api/src/main/java/com/develop/photoapp/entity/UserEntity.java
@@ -0,0 +1,43 @@
+package com.develop.photoapp.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserEntity implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 734422273758762280L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 50)
+    private String firstName;
+
+    @Column(nullable = false, length = 50)
+    private String lastName;
+
+    @Column(nullable = false, length = 120, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String userId;
+
+    @Column(nullable = false, unique = true)
+    private String encryptedPassword;
+}
