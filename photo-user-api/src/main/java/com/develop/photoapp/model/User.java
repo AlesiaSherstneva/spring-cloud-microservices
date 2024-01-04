e438e4fc38d9e7bf3efc1e160985dad5afa49de2
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-01-04 15:03:29 +0300

    Add user POJO class with fields validation

---
+++ photo-user-api/src/main/java/com/develop/photoapp/model/User.java
@@ -0,0 +1,27 @@
+package com.develop.photoapp.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    @NotNull(message = "First name cannot be null")
+    @Size(min = 2, message = "First name must not be less than 2 characters")
+    private String firstName;
+
+    @NotNull(message = "Last name cannot be null")
+    @Size(min = 2, message = "First name must not be less than 2 characters")
+    private String lastName;
+
+    @NotNull(message = "Password cannot be null")
+    @Size(min = 8, max = 16, message = "Password must be from 8 to 16 characters")
+    private String password;
+
+    @NotNull(message = "Email cannot be null")
+    @Email
+    private String email;
+}
