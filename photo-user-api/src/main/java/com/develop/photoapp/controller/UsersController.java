92bad8c352d586a0c3a879e398d2bc19ca9d828e
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-01-04 18:54:30 +0300

    Create POST method to the controller, inject repo and mapper

--- photo-user-api/src/main/java/com/develop/photoapp/controller/UsersController.java
+++ photo-user-api/src/main/java/com/develop/photoapp/controller/UsersController.java
@@ -1,8 +1,16 @@
 package com.develop.photoapp.controller;
 
+import com.develop.photoapp.model.User;
+import com.develop.photoapp.service.UsersService;
+import com.develop.photoapp.shared.UserDTO;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UsersController {
     private final Environment environment;
+    private final UsersService usersService;
+    private final ModelMapper modelMapper;
 
     @GetMapping("/status/check")
     public String status() {
         return "Working on port: " + environment.getProperty("local.server.port");
+    }
+
+    @PostMapping
+    public String createUser(@Valid @RequestBody User userDetails) {
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserDTO userDTO = modelMapper.map(userDetails, UserDTO.class);
+
+        usersService.createUser(userDTO);
+        return "Create user method is called";
     }
 }
