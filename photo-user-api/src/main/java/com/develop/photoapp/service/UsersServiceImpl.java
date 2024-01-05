95165268ad8c9d790ae671767a0d850022020215
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-01-05 03:54:33 +0300

    Encrypt user password

--- photo-user-api/src/main/java/com/develop/photoapp/service/UsersServiceImpl.java
+++ photo-user-api/src/main/java/com/develop/photoapp/service/UsersServiceImpl.java
@@ -5,6 +5,7 @@ import com.develop.photoapp.repository.UsersRepository;
 import com.develop.photoapp.shared.UserDTORequest;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -14,13 +15,14 @@ import java.util.UUID;
 public class UsersServiceImpl implements UsersService {
     private final UsersRepository usersRepository;
     private final ModelMapper modelMapper;
+    private final BCryptPasswordEncoder encoder;
 
     @Override
     public UserDTORequest createUser(UserDTORequest userDetails) {
         userDetails.setUserId(UUID.randomUUID().toString());
+        userDetails.setEncryptedPassword(encoder.encode(userDetails.getPassword()));
 
         UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
-        userEntity.setEncryptedPassword("test");
         usersRepository.save(userEntity);
 
         return modelMapper.map(userEntity, UserDTORequest.class);
