96c5ec7a2b624b0e502501a9f6197e7701753eac
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-01-05 22:18:51 +0300

    Configure security filter chain

--- photo-user-api/src/main/java/com/develop/photoapp/security/WebSecurity.java
+++ photo-user-api/src/main/java/com/develop/photoapp/security/WebSecurity.java
@@ -1,23 +1,48 @@
 package com.develop.photoapp.security;
 
+import com.develop.photoapp.service.UsersService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurity {
+    private final UsersService usersService;
+    private final Environment environment;
+    private final BCryptPasswordEncoder encoder;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((auth) -> auth
-                .requestMatchers("/users/**", "/h2-console/**")
-                .permitAll());
-        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
-        http.csrf(AbstractHttpConfigurer::disable);
-        return http.build();
+        AuthenticationManagerBuilder authBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authBuilder.userDetailsService(usersService).passwordEncoder(encoder);
+
+        AuthenticationManager authManager = authBuilder.build();
+
+        return http
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authenticationManager(authManager)
+                .addFilter(getAuthenticationFilter(authManager))
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/users/**", "/h2-console/**")
+                        .permitAll())
+                .build();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter(AuthenticationManager authManager) {
+        AuthenticationFilter filter = new AuthenticationFilter(usersService, environment);
+        filter.setAuthenticationManager(authManager);
+        return filter;
     }
 }
