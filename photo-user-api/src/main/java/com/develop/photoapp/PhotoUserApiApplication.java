2438685ef1a877bfcd7c7abd36cf54b7af551b89
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-01-05 03:50:15 +0300

    Create bean of bcrypt password encoder

--- photo-user-api/src/main/java/com/develop/photoapp/PhotoUserApiApplication.java
+++ photo-user-api/src/main/java/com/develop/photoapp/PhotoUserApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -19,5 +20,10 @@ public class PhotoUserApiApplication {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         return modelMapper;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
