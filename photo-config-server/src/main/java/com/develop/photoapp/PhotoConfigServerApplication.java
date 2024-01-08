636680e58ba8b064565254189b0cd824bf5375ec
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-01-08 09:41:40 +0300

    Create new project for Config server

---
+++ photo-config-server/src/main/java/com/develop/photoapp/PhotoConfigServerApplication.java
@@ -0,0 +1,11 @@
+package com.develop.photoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoConfigServerApplication.class, args);
+    }
+}
---
+++ photo-config-server/src/test/java/com/develop/photoapp/PhotoConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develop.photoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
