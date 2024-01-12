440dd234a2ef83f23fb8bf9c0fe7a81809c0f0dc
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-01-12 22:40:42 +0300

    Add load balanced bean of rest template to the users api

--- photo-user-api/src/main/java/com/develop/photoapp/PhotoUserApiApplication.java
+++ photo-user-api/src/main/java/com/develop/photoapp/PhotoUserApiApplication.java
@@ -5,8 +5,10 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -25,5 +27,11 @@ public class PhotoUserApiApplication {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
