7e0e6ce95ea0620bcee315cde22b46af5bbc2f3d
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-01-12 23:15:22 +0300

    Make users service calling albums service

--- photo-user-api/src/main/java/com/develop/photoapp/service/UsersServiceImpl.java
+++ photo-user-api/src/main/java/com/develop/photoapp/service/UsersServiceImpl.java
@@ -2,16 +2,24 @@ package com.develop.photoapp.service;
 
 import com.develop.photoapp.entity.UserEntity;
 import com.develop.photoapp.repository.UsersRepository;
+import com.develop.photoapp.shared.AlbumDTOResponse;
 import com.develop.photoapp.shared.UserDTORequest;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
@@ -20,6 +28,8 @@ public class UsersServiceImpl implements UsersService {
     private final UsersRepository usersRepository;
     private final ModelMapper modelMapper;
     private final BCryptPasswordEncoder encoder;
+    private final RestTemplate restTemplate;
+    private final Environment environment;
 
     @Override
     public UserDTORequest createUser(UserDTORequest userDetails) {
@@ -47,7 +57,17 @@ public class UsersServiceImpl implements UsersService {
         if (userEntity == null) {
             throw new UsernameNotFoundException("User not found");
         }
-        return modelMapper.map(userEntity, UserDTORequest.class);
+
+        String albumsUrl = String.format(Objects.requireNonNull(environment.getProperty("albums.url")), userId);
+        ResponseEntity<List<AlbumDTOResponse>> albumsListResponse = restTemplate.exchange(
+                albumsUrl, HttpMethod.GET, null, new ParameterizedTypeReference<>() {
+                });
+        List<AlbumDTOResponse> albums = albumsListResponse.getBody();
+
+        UserDTORequest userDTORequest = modelMapper.map(userEntity, UserDTORequest.class);
+        userDTORequest.setAlbums(albums);
+
+        return userDTORequest;
     }
 
     @Override
