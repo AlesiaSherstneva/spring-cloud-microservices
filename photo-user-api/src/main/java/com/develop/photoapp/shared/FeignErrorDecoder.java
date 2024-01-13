769c0cb2ef98cac2b13da356677d0a1fe9d999d7
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-01-13 22:59:38 +0300

    Enable Feign error decoder

---
+++ photo-user-api/src/main/java/com/develop/photoapp/shared/FeignErrorDecoder.java
@@ -0,0 +1,27 @@
+package com.develop.photoapp.shared;
+
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class FeignErrorDecoder implements ErrorDecoder {
+    @Override
+    public Exception decode(String methodKey, Response response) {
+        switch (response.status()) {
+            case 400: {
+                break;
+            }
+            case 404: {
+                if (methodKey.contains("getAlbums")) {
+                    return new ResponseStatusException(HttpStatus.valueOf(response.status()),
+                            "User's albums are not available");
+                }
+                break;
+            }
+            default:
+                return new Exception(response.reason());
+        }
+        return null;
+    }
+}
