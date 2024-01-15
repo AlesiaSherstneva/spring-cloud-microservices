0cfe7755b679eb0b0224bfa54b3065f9c14c5446
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-01-15 21:59:20 +0300

    Change fallback class to fallback factory class

--- photo-user-api/src/main/java/com/develop/photoapp/client/AlbumServiceClient.java
+++ photo-user-api/src/main/java/com/develop/photoapp/client/AlbumServiceClient.java
@@ -1,22 +1,43 @@
 package com.develop.photoapp.client;
 
 import com.develop.photoapp.shared.AlbumDTOResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@FeignClient(name = "albums-ws", fallback = AlbumsFallback.class)
+// @FeignClient(name = "albums-ws", fallback = AlbumsFallback.class)
+@FeignClient(name = "albums-ws", fallbackFactory = AlbumsFallbackFactory.class)
 public interface AlbumServiceClient {
     @GetMapping("/users/{id}/albums")
     List<AlbumDTOResponse> getAlbums(@PathVariable String id);
 }
 
+/*
 @Component
 class AlbumsFallback implements AlbumServiceClient {
+    @Override
+    public List<AlbumDTOResponse> getAlbums(String id) {
+        return new ArrayList<>();
+    }
+}
+*/
+
+class AlbumsFallbackFactory implements FallbackFactory<AlbumServiceClient> {
+    @Override
+    public AlbumServiceClient create(Throwable cause) {
+        return new AlbumsServiceClientFallback(cause);
+    }
+}
+
+@RequiredArgsConstructor
+class AlbumsServiceClientFallback implements AlbumServiceClient {
+    private final Throwable cause;
+
     @Override
     public List<AlbumDTOResponse> getAlbums(String id) {
         return new ArrayList<>();
