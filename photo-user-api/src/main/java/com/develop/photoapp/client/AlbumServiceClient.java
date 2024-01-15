7b77c121f21106a417b696fed5fd09510c995a95
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-01-15 22:22:55 +0300

    Enable logging for Feign exceptions

--- photo-user-api/src/main/java/com/develop/photoapp/client/AlbumServiceClient.java
+++ photo-user-api/src/main/java/com/develop/photoapp/client/AlbumServiceClient.java
@@ -1,9 +1,12 @@
 package com.develop.photoapp.client;
 
 import com.develop.photoapp.shared.AlbumDTOResponse;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -27,6 +30,7 @@ class AlbumsFallback implements AlbumServiceClient {
 }
 */
 
+@Component
 class AlbumsFallbackFactory implements FallbackFactory<AlbumServiceClient> {
     @Override
     public AlbumServiceClient create(Throwable cause) {
@@ -34,12 +38,19 @@ class AlbumsFallbackFactory implements FallbackFactory<AlbumServiceClient> {
     }
 }
 
+@Slf4j
 @RequiredArgsConstructor
 class AlbumsServiceClientFallback implements AlbumServiceClient {
     private final Throwable cause;
 
     @Override
     public List<AlbumDTOResponse> getAlbums(String id) {
+        if (cause instanceof FeignException && ((FeignException) cause).status() == 404) {
+            log.error(String.format("404 error took place when getAlbums() was called " +
+                    "with userId: %s. The error message: %s", id, cause.getLocalizedMessage()));
+        } else {
+            log.error(String.format("Other error took place: %s", cause.getLocalizedMessage()));
+        }
         return new ArrayList<>();
     }
 }
