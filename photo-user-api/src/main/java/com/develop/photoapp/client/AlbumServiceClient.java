6f0cad8c2b07e27fa081f31ed80cc91f21cad3e9
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-01-15 20:52:14 +0300

    Create fallback class for album service

--- photo-user-api/src/main/java/com/develop/photoapp/client/AlbumServiceClient.java
+++ photo-user-api/src/main/java/com/develop/photoapp/client/AlbumServiceClient.java
@@ -2,13 +2,23 @@ package com.develop.photoapp.client;
 
 import com.develop.photoapp.shared.AlbumDTOResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@FeignClient(name = "albums-ws")
+@FeignClient(name = "albums-ws", fallback = AlbumsFallback.class)
 public interface AlbumServiceClient {
     @GetMapping("/users/{id}/albums")
     List<AlbumDTOResponse> getAlbums(@PathVariable String id);
+}
+
+@Component
+class AlbumsFallback implements AlbumServiceClient {
+    @Override
+    public List<AlbumDTOResponse> getAlbums(String id) {
+        return new ArrayList<>();
+    }
 }
