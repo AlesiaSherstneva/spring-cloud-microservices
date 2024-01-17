0e4c6d0fdcbe625e01adbb2bd57c4bd289689b2f
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-01-17 22:49:57 +0300

    Enable basic security to Eureka service

---
+++ photo-discovery-service/src/main/java/com/develop/photoapp/WebSecurity.java
@@ -0,0 +1,25 @@
+package com.develop.photoapp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest()
+                        .authenticated())
+                .httpBasic(withDefaults())
+                .build();
+    }
+}
