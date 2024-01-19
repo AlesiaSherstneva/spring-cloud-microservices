019869689275105ba6216a331db920e4ff6dc665
Alesia Sherstneva <alesiasherstneva@gmail.com>
2024-01-19 23:04:15 +0300

    Delete unused apps

--- mobile-app-ws/src/main/java/com/develop/app/ws/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.develop.app.ws.service;
-
-import com.develop.app.ws.model.UserDetails;
-import com.develop.app.ws.model.UserRest;
-
-public interface UserService {
-    UserRest createUser(UserDetails userDetails);
-}
